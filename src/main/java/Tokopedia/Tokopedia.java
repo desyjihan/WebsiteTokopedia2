cc8ca4a802410545bf3723b053cc1cf229ecf1d8
desyjihan <desyjihan7@gmail.com>
2022-10-08 08:46:39 +0700

    login

--- src/main/java/Tokopedia/Tokopedia.java
+++ src/main/java/Tokopedia/Tokopedia.java
@@ -1,12 +1,13 @@
 package Tokopedia;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tokopedia {
+	
 	public static void main(String[] args) {
 	
 	System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
@@ -14,18 +15,25 @@ public class Tokopedia {
     driver.get("https://www.tokopedia.com");
 	driver.manage().window().maximize();
 	
-	//==========Search Item==========//
+	/* LOGIN TOKOPEDIA */
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[5]/button[1]")).click();
+	delay(3);
 	driver.findElement(By.xpath("//*[@id=\"email-phone\"]")).sendKeys("jihandesy12@gmail.com");
 	driver.findElement(By.xpath("//*[@id=\"email-phone-submit\"]")).click();
+	delay(3);
 	driver.findElement(By.xpath("//*[@id=\"password-input\"]")).sendKeys("desyjihan12");
-	driver.findElement(By.xpath("//span[@class='css-16s5os-unf-checkbox__area e3y1k2n1']")).click();
-	driver.findElement(By.xpath("//span[@aria-label='login-button']")).click();
+	delay(2);
+	driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[2]/form/button/span/span")).click();
+	delay(10);
+	
+	/* SEARCH ITEM */
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div")).click();
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div/input")).sendKeys("Flatshoes");
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div/input")).sendKeys(Keys.ENTER);
 	delay(3);
 //	scroll(200);
+	
+	/* CHOOSE ITEM */
 	driver.findElement(By.xpath("//*[@id=\"zeus-root\"]/div/div[2]/div/div[2]/div[3]/div[2]/div[2]")).click();
 	
 	delay(3);
