8c5eee758bc2e970bb8107b31717e526d527d122
desyjihan <desyjihan7@gmail.com>
2022-10-08 09:34:07 +0700

    search item

--- src/main/java/Tokopedia/Tokopedia.java
+++ src/main/java/Tokopedia/Tokopedia.java
@@ -1,42 +1,49 @@
 package Tokopedia;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tokopedia {
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	
 	System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
     driver.get("https://www.tokopedia.com");
 	driver.manage().window().maximize();
-	
-	/* LOGIN TOKOPEDIA */
-	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[5]/button[1]")).click();
-	delay(3);
-	driver.findElement(By.xpath("//*[@id=\"email-phone\"]")).sendKeys("jihandesy12@gmail.com");
-	driver.findElement(By.xpath("//*[@id=\"email-phone-submit\"]")).click();
-	delay(3);
-	driver.findElement(By.xpath("//*[@id=\"password-input\"]")).sendKeys("desyjihan12");
-	delay(2);
-	driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[2]/form/button/span/span")).click();
-	delay(10);
+	JavascriptExecutor js = (JavascriptExecutor) driver;
 	
 	/* SEARCH ITEM */
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div")).click();
+	System.out.println("Button Search");
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div/input")).sendKeys("Flatshoes");
+	System.out.println("Input Search Item");
+	delay(2);
 	driver.findElement(By.xpath("//*[@id=\"header-main-wrapper\"]/div[2]/div[2]/div[1]/div/div/div/input")).sendKeys(Keys.ENTER);
+	System.out.println("Success Input Item");
 	delay(3);
-//	scroll(200);
+
+	js.executeScript("window,scrollBy(0,400)");
+	Thread.sleep(400);
 	
 	/* CHOOSE ITEM */
-	driver.findElement(By.xpath("//*[@id=\"zeus-root\"]/div/div[2]/div/div[2]/div[3]/div[2]/div[2]")).click();
-	
+	driver.findElement(By.xpath("//img[@alt='FARADELA Xandria Flatshoes Wanita Cute Mode Premium F01-14.3']")).click();
+	System.out.println("Show Item");
 	delay(3);
+	
+	js.executeScript("window,scrollBy(0,100)");
+	Thread.sleep(400);
+	delay(3);
+	
+	driver.findElement(By.xpath("//*[@id=\"pdpVariantContainer\"]/div[1]/div[1]/div/div[4]")).click();
+	System.out.println("Choose Color");
+//	delay(3);
+	driver.findElement(By.xpath("//*[@id=\"pdpVariantContainer\"]/div[1]/div[2]/div/div[3]")).click();
+	System.out.println("Choose Size");
+	
+	delay(5);
 	driver.quit();
 	}
 	
@@ -49,10 +56,4 @@ public class Tokopedia {
 		}
 
 	}
-	
-//	static void scroll(int vertikal) {
-//		JavascriptExecutor js =(JavascriptExecutor) driver;
-//		js.executeScript("window.scrollBy(0,"+vertikal+")");
-//	}
-
 }
